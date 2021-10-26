758df315aa7c7b224f41cbd10c6daddfe9fd4e3a
mihatomsic17 <mihatomsic17@gmail.com>
2021-10-26 10:26:50 +0200

    zagovor

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.View.inflate
 import android.widget.EditText
+import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import com.example.myfirstapp.databinding.ActivityMainBinding
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    fun onClickAdd (view: View?) {
-        val shopName = findViewById<EditText>(R.id.shopNameText).text.toString()
-        val serialNum = findViewById<EditText>(R.id.serialText).text.toString()
+    fun onClickAdd (view: View) {
+        val shopName = binding.shopNameText.text.toString()
+        val serialNum = binding.serialText.text.toString()
         listOfShops.add(Shop(shopName, serialNum.toInt()))
-        findViewById<EditText>(R.id.shopNameText).text.clear()
-        findViewById<EditText>(R.id.serialText).text.clear()
+        binding.shopNameText.text.clear()
+        binding.serialText.text.clear()
+
     }
 
     fun onClickInfo (view: View) {
